c60106f99e904c2628890368352d9f68f9dc8e46
m9898989 <151790803+m9898989@users.noreply.github.com>
2023-12-09 00:28:14 -0500

    Add files via upload
    Code de l'application incluant toutes les fonctions et méthodes implémentées dans le projet.

--- app.py
+++ app.py
@@ -1,182 +1,229 @@
-import streamlit as st
-from contribution.contribution_functions import submit_contribution, submit_contribution_author, submit_additional_authors, g_main_topics,show_submitted_informations
-#from save_data.save_data_functions import save_contribution_data, show_submitted_information
-from confirmation_email.email_functions import send_confirmation_email
-from save_data.save_data_functions import save_contribution_data_snowflake
-from contribution.registration_functions import save_payment_info, save_registration_info, confirm_email,save_to_database
-import re
-
-# Fonction pour la page d'inscription
-
-def show_inscription_page():
-    # Définir la réponse au défi "To prove you are not a robot"
-    challenge_response = "13"  # La réponse correcte est "13"
-
-    # Définir un titre pour votre application Streamlit
-    st.title("IARIA Congress 2023 - Submit a New Contribution")
-
-    # Créer le formulaire de soumission
-    contribution_title, track_preference, main_topic, contribution_type, content_type, challenge_answer = submit_contribution()
-
-    # Formulaire pour l'auteur principal
-    st.subheader("Author Information (Contact Author):")
-    contact_author, author_email, first_name, last_name, institution, country = submit_contribution_author()
-
-    # Définir le motif pour vérifier le format de l'e-mail
-    email_pattern = r'^[\w\.-]+@[\w\.-]+(\.\w+)+$'
-
-    # Liste pour les autres auteurs
-    st.subheader("Additional Authors:")
-    num_additional_authors, additional_authors = submit_additional_authors()
-
-    # Champ pour l'abstract
-    abstract = st.text_area("Abstract")
-
-    # Liste des topics
-    st.subheader("Topics")
-    main_topic_options = g_main_topics()
-    selected_topics = st.multiselect("Select Topics", main_topic_options)
-    confirmation_code = send_confirmation_email(author_email, first_name)
-    # Boutons Submit et Reset
-    if st.button("Submit Contribution"):
-        if (
-            contribution_title
-            and track_preference
-            and main_topic
-            and contribution_type
-            and content_type
-            and challenge_answer == challenge_response
-            and first_name
-            and last_name
-            and institution
-            and re.match(email_pattern, author_email)  # Vérification de l'e-mail de l'auteur principal
-        ):
-            try:
-                # Enregistrez les données de la contribution dans la base de données ou un autre système de stockage
-                save_contribution_data_snowflake(contribution_title, track_preference, main_topic, contribution_type, abstract, content_type,author_email, confirmation_code)
-
-                # Affichez les informations soumises en bas de la page
-                show_submitted_informations(contribution_title, track_preference, main_topic, contribution_type, content_type,
-                                            author_email, first_name, last_name, institution, country,
-                                            additional_authors, abstract, selected_topics)
-
-                # Envoyez l'e-mail de confirmation
-                send_confirmation_email(author_email, first_name)
-
-                st.success(f"Contribution '{contribution_title}' submitted successfully.")
-                st.info("Please check your email for further instructions.")
-            except Exception as e:
-                st.error(f"An error occurred while processing the contribution: {str(e)}")
-        else:
-            st.error("Please fill in all required fields and pass the challenge.")
-
-    # Bouton Reset
-    if st.button("Reset"):
-        st.experimental_rerun()
-
-# Fonction pour la page d'inscription
-import streamlit as st
-
-def show_registration_page():
-    st.title("Registration and Payment")
-    st.title("Registration Form")
-
-    # Formulaire d'inscription
-    email = st.text_input("Email")
-    title = st.text_input("Title")
-    first_name = st.text_input("First Name")
-    last_name = st.text_input("Last Name")
-    institution = st.text_input("Institution")
-    country = st.text_input("Country")
-    street_address = st.text_input("Street Address")
-    city = st.text_input("City")
-    state = st.text_input("State")
-    postal_code = st.text_input("Postal Code")
-    phone = st.text_input("Phone")
-    fax = st.text_input("Fax")
-    vat = st.text_input("VAT")
-    about_conference = st.text_area("About Conference")
-
-    # Bouton de soumission du formulaire
-    if st.button("Submit"):
-        save_to_database(email, title, first_name, last_name, institution, country,
-                          street_address, city, state, postal_code, phone, fax, vat, about_conference)
-        st.success("Registration submitted successfully!")
-    # Section de confirmation de l'email0000
-    st.header("Email Confirmation")
-
-    # Entrée du code de confirmation
-    confirmation_code_input = st.text_input("Enter Confirmation Code")
-    email_input = st.text_input("Enter Email for Confirmation")
-
-    # Bouton de confirmation
-    if st.button("Confirm Email"):
-        confirm_email(confirmation_code_input, email_input)
-
-    # Section d'inscription
-    st.header("Registration")
-
-    selected_registration = st.selectbox(
-        "Select Registration",
-        [
-            "Select Registration",
-            "Registration iaria fellow - 645 EUR",
-            "Registration Full time student - 645 EUR",
-            "Registration Academic - 695 EUR",
-            "Registration Industry - 835 EUR"
-        ]
-    )
-
-    # Section d'options supplémentaires
-    st.header("Additional Options")
-    extra_pages = st.selectbox("Extra Pages (105 EUR per page over 6 pages)", list(range(7)), format_func=lambda x: 'No extra pages' if x == 0 else f"{x} extra pages")
-    extra_gala_dinners = st.selectbox("Extra Gala Dinners (95 EUR, needed for accompanying persons)", list(range(4)), format_func=lambda x: 'No extra dinners' if x == 0 else f"{x} extra dinner(s)")
-
-    # Bouton d'inscription
-    if st.button("Submit Registration"):
-        save_registration_info(confirmation_code_input, selected_registration, extra_pages, extra_gala_dinners)
-
-    # Section de paiement
-    st.title("Payment")
-
-    # Sélection du mode de paiement
-    payment_method = st.radio("Select Payment Method", ["Visa", "MasterCard", "Discover", "American Express", "Bank Transfer"])
-
-    if payment_method != "Bank Transfer":
-        st.subheader("Card Details")
-        card_number = st.text_input("Card Number")
-        expiration_date = st.text_input("Expiration Date (MM/YY)")
-        cvv = st.text_input("CVV")
-
-    # Bouton de paiement
-    if st.button("Submit Payment"):
-        if payment_method == "Bank Transfer":
-            st.success("Your bank transfer has been processed.")
-        else:
-            save_payment_info(payment_method, card_number, expiration_date, cvv)
-
-# Note: Assurez-vous d'avoir les fonctions confirm_email, save_registration_info, et save_payment_info définies dans votre code.
-
-# Fonction principale
-def main():
-    # Titre de l'application
-    st.title("ACADEMIC PUBLISH")
-
-    # Titre de la barre latérale
-    st.sidebar.title("CHOOSE YOUR PAGE")
-
-    # Liste déroulante pour la navigation sur la gauche
-    selected_page = st.sidebar.selectbox("", ["Home", "SUBMIT A CONTRIBUTION", "REGISTER"])
-
-    # Afficher le contenu en fonction de la page sélectionnée
-    if selected_page == "Home":
-        st.write("WELCOME TO YOUR PLATFORM TO SUBMIT YOUR SCIENTIFIC ARTICLE")
-    elif selected_page == "SUBMIT A CONTRIBUTION":
-        show_inscription_page()
-    elif selected_page == "REGISTER":
-        show_registration_page()
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import jsonify
+import json
+from datetime import date
 
 
-# Exécutez l'application
-if __name__ == "__main__":
-    main()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@127.0.0.1/omnivox'
+app.config['SECRET_KEY'] = 'votre_secret_key_ici'
+db = SQLAlchemy(app)
+
+
+class CoursGestion(db.Model):
+    __tablename__ = 'cours_gestion'
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(100))
+    programme = db.Column(db.String(100))
+    description = db.Column(db.String(500))
+    nombre_heures = db.Column(db.Integer)
+    prof = db.Column(db.String(100))
+    salle = db.Column(db.String(100))
+    heure = db.Column(db.Time)
+    date_limite = db.Column(db.Date,nullable=True)
+
+
+class Etudiant(db.Model):
+    __tablename__ = 'etudiant'
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(50))
+    prenom = db.Column(db.String(50))
+    code_permanent = db.Column(db.String(20), unique=True)
+    mot_de_passe = db.Column(db.String(80))
+    cours = db.relationship('CoursGestion', secondary='association', backref='etudiants')
+
+class Cours(db.Model):
+    __tablename__ = 'cours'
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(100))
+    raison = db.Column(db.String(200))
+
+
+association = db.Table('association',
+    db.Column('etudiant_id', db.Integer, db.ForeignKey('etudiant.id')),
+    db.Column('cours_id', db.Integer, db.ForeignKey('cours_gestion.id'))
+)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/inscription', methods=['GET', 'POST'])
+def inscription():
+    if request.method == 'POST':
+        nom = request.form.get('nom')
+        prenom = request.form.get('prenom')
+        code_permanent = request.form.get('code_permanent')
+        mot_de_passe = generate_password_hash(request.form.get('mot_de_passe'))
+        nouvel_etudiant = Etudiant(nom=nom, prenom=prenom, code_permanent=code_permanent, mot_de_passe=mot_de_passe)
+        db.session.add(nouvel_etudiant)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('inscription.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        code_permanent = request.form.get('code_permanent')
+        mot_de_passe = request.form.get('mot_de_passe')
+        etudiant = Etudiant.query.filter_by(code_permanent=code_permanent).first()
+        if etudiant and check_password_hash(etudiant.mot_de_passe, mot_de_passe):
+            session['etudiant_id'] = etudiant.id
+            return redirect(url_for('dashboard'))
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+    return render_template('dashboard.html')
+
+@app.route('/choisir_cours')
+def choisir_cours():
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+    cours_disponibles = CoursGestion.query.all()
+    return render_template('ajout_cours.html', cours_disponibles=cours_disponibles)
+
+@app.route('/ajout_cours', methods=['POST'])
+def ajouter_cours_etudiant():
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+
+    titre_cours = request.form.get('titre')
+    etudiant = Etudiant.query.get(session['etudiant_id'])
+
+    cours_existant = CoursGestion.query.filter_by(titre=titre_cours).first()
+    if cours_existant:
+        etudiant.cours.append(cours_existant)
+        db.session.commit()
+        flash('Le cours a été ajouté avec succès.')
+    else:
+        flash('Le cours demandé n\'existe pas.')
+
+    return redirect(url_for('mes_cours'))
+
+
+
+@app.route('/ajouter_cours', methods=['GET'])
+def afficher_formulaire_ajout_cours():
+    
+    return render_template('ajout_cours.html')
+
+
+
+@app.route('/mes_cours')
+def mes_cours():
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+    etudiant = db.session.get(Etudiant, session['etudiant_id'])
+
+    return render_template('mes_cours.html', cours=etudiant.cours)
+
+
+@app.route('/abandonner_cours/<int:cours_id>')
+def abandonner_cours(cours_id):
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+
+    etudiant = Etudiant.query.get(session['etudiant_id'])
+    db.session.refresh(etudiant) 
+    cours = CoursGestion.query.get(cours_id)
+
+    
+    if cours.date_limite and cours.date_limite < date.today():
+        flash("Attention, vous aurez une mention d'échec si vous abandonnez ce cours après la date limite!", "warning")
+        return redirect(url_for('mes_cours')) 
+    cours = CoursGestion.query.get(cours_id)
+    etudiant.cours.remove(cours)
+    db.session.commit()
+    flash("Cours abandonné avec succès.", "success")
+    return redirect(url_for('mes_cours'))
+
+
+@app.route('/mon_emploi')
+def mon_emploi():
+    if 'etudiant_id' not in session:
+        return redirect(url_for('login'))
+
+    etudiant = Etudiant.query.get(session['etudiant_id'])
+    cours_inscrits = etudiant.cours
+
+    cours_data = []
+    for cours in cours_inscrits:
+        cours_data.append({
+            'title': cours.titre,
+            'start': cours.heure.isoformat()
+        })
+
+    cours_data_js = jsonify(cours_data).data.decode("utf-8")
+    return render_template('emploi.html', cours_data_js=cours_data_js)
+
+
+
+@app.route('/login_admin', methods=['GET', 'POST'])
+def login_admin():
+    if request.method == 'POST':
+        code_admin = request.form.get('code_admin')
+        mot_de_passe = request.form.get('mot_de_passe')
+        if code_admin == "admin" and mot_de_passe == "1111":
+            session['admin_id'] = code_admin
+            return redirect(url_for('admin'))
+    return render_template('login_admin.html')
+
+@app.route('/ajouter_cours_admin', methods=['GET', 'POST'])
+def ajouter_cours_admin():
+    if 'admin_id' not in session:
+        return redirect(url_for('login_admin'))
+    if request.method == 'POST':
+        titre = request.form.get('titre')
+        programme = request.form.get('programme')
+        description = request.form.get('description')
+        nombre_heures = request.form.get('nombre_heures')
+        prof = request.form.get('prof')
+        salle = request.form.get('salle')
+        heure = request.form.get('heure')
+        date_limite = request.form.get('date_limite')  
+        
+        if not date_limite:
+            date_limite = None
+
+        nouveau_cours = CoursGestion(
+            titre=titre, 
+            programme=programme, 
+            description=description, 
+            nombre_heures=nombre_heures, 
+            prof=prof, 
+            salle=salle, 
+            heure=heure,
+            date_limite=date_limite
+        )
+        db.session.add(nouveau_cours)
+        db.session.commit()
+        return redirect(url_for('admin'))
+    return render_template('ajouter_cours_admin.html')
+
+
+@app.route('/admin')
+def admin():
+    if 'admin_id' not in session:
+        return redirect(url_for('login_admin'))
+    return render_template('ajouter_cours_admin.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
